7418ba129dc963386c0073995aaa6b6923371d51
masonkobilka <masonkobilka@gmail.com>
2025-09-16 00:02:01 -0500

    add word length func

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+def word_count(text):
+    text_list = text.split()
+    return len(text_list)
+
 def get_books_text(filepath):
     with open(filepath) as f:
         text = f.read()
     return text
 
 def main():
-    print(get_books_text("./books/frankenstein.txt"))
+    text_length = word_count(get_books_text("./books/frankenstein.txt"))
+    print(f"{text_length} words found in the document")
 
 if __name__ == "__main__":
     main()
